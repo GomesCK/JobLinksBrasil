78a87222f4ec6b7d1b24789611f4d480f5b9fa8c
GomesCK <afonsogomes.3.vip@gmail.com>
2024-10-30 21:35:58 -0300

    Add atualização

--- src/main/java/br/com/joblinks/controller/RegisterController.java
+++ src/main/java/br/com/joblinks/controller/RegisterController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/series")
-public class SiteController {
+@RequestMapping("/v1/users")
+public class RegisterController {
 
    @Autowired
    private SerieService servico;
